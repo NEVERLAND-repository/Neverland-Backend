829841d8f407d22e30e8d4585f24f018037f1bf6
Jim Ezesinachi <ezesinachijim@gmail.com>
2022-03-27 10:02:09 +0100

    fixed packages

--- src/db/index.js
+++ src/db/index.js
@@ -3,15 +3,15 @@ const mongoose = require('mongoose');
 const connect = async (url) => {
     // Mongoose and Server start up
     try {
-        await mongoose.connect(url,
+        mongoose.connect(url,
             {
                 useNewUrlParser: true,
                 useUnifiedTopology: true
-            });    
+            }); 
     } catch (error) {
-        console.log(`Database connection failed \n ${error}`)
+        console.log(`Database connection failed \n ${error}`);
     }
     console.log('Database connection is successful');
 }
 
-exports.connect = connect
+exports.connect = connect;
