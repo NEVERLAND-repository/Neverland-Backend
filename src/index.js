0710d4c739d1e94b21c2a8abdf4369c4903d375e
Ekene Nwobodo <nwobodoe71@gmail.com>
2022-03-29 15:01:27 +0100

    Configure eslint, database
    Signed-off-by: Ekene Nwobodo <nwobodoe71@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 const dotenv = require('dotenv');
+const path = require('path');
 const app = require('./app');
 const { connect } = require('./db');
 
 // configure dotenv and port
-dotenv.config();
+dotenv.config({ path: path.join(__dirname, '/.env') });
 const port = process.env.PORT || 8800;
+const DB = process.env.MONGODB_URL.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
 
 const start = async (_port, _url, _app) => {
-  await connect(_url);
-  _app.listen(_port, () => console.log(`Server is running on Port: ${_port}`));
+  try {
+    await connect(_url);
+    _app.listen(_port, () =>
+      console.log(`Server is running on Port: ${_port}`));
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-start(port, process.env.MONGODB_URL, app);
+start(port, DB, app);
