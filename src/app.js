96325cd8ab4a5d13a84890ec9047952ea2b98e48
cyrilchukwuebuka <muofunanya3@gmail.com>
2022-03-29 09:51:14 +0100

    configured eslint

--- src/app.js
+++ src/app.js
@@ -4,11 +4,11 @@ const helmet = require('helmet');
 const morgan = require('morgan');
 
 // create an express app
-const app = express()
+const app = express();
 
-//middlewares
+// middlewares
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}))
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(helmet());
 app.use(morgan('dev'));
--- src/db/index.js
+++ src/db/index.js
@@ -1,17 +1,19 @@
 const mongoose = require('mongoose');
 
 const connect = async (url) => {
-    // Mongoose and Server start up
-    try {
-        await mongoose.connect(url,
-            {
-                useNewUrlParser: true,
-                useUnifiedTopology: true
-            });    
-    } catch (error) {
-        console.log(`Database connection failed \n ${error}`)
-    }
-    console.log('Database connection is successful');
-}
+  // Mongoose and Server start up
+  try {
+    mongoose.connect(
+      url,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      },
+    );
+  } catch (error) {
+    console.log(`Database connection failed \n ${error}`);
+  }
+  console.log('Database connection is successful');
+};
 
-exports.connect = connect
+exports.connect = connect;
