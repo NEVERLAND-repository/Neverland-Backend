cf9737c0c98a1ccd9b315e2ed7df8a8afc4c339b
Jim Ezesinachi <ezesinachijim@gmail.com>
2022-03-30 21:06:13 +0100

    fixed db conn and lint file

--- src/db/index.js
+++ src/db/index.js
@@ -1,22 +1,14 @@
 const mongoose = require('mongoose');
 
-const connect = (url) => {
-  // Mongoose and Server start up
-  try {
-    mongoose.connect(
-      url,
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false,
-        useCreateIndex: true,
-      },
-    );
-  } catch (error) {
-    // eslint-disable-next-line no-console
-    console.log(`Database connection failed \n ${error}`);
-  }
-  // eslint-disable-next-line no-console
+const connect = async (url) => {
+  await mongoose.connect(
+    url,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    },
+  );
+
   console.log('Database connection is successful');
 };
 
--- src/index.js
+++ src/index.js
@@ -1,34 +1,26 @@
 const dotenv = require('dotenv');
-const path = require('path');
 const asyncHandler = require('express-async-handler');
 const app = require('./app');
-const { connect } = require('./db');
+const connect = require('./db');
 
 // configure dotenv and port
-dotenv.config({ path: path.join(__dirname, '/.env') });
+dotenv.config();
 const port = process.env.PORT || 8800;
-const DB = process.env.MONGODB_URL.replace(
-  '<PASSWORD>',
-  process.env.DATABASE_PASSWORD,
-);
+const DB = process.env.MONGODB_URL;
 
 const start = asyncHandler(async (_port, _url, _app) => {
   try {
     await connect(_url);
     _app.listen(_port, () =>
-      // eslint-disable-next-line no-console
-      console.log(`Server is running on Port: ${_port}`));
+      console.log(`Server is running on port: ${_port}`));
   } catch (error) {
-    // eslint-disable-next-line no-console
     console.log(error);
   }
 });
 
 // unhandled rejection
 process.on('unhandledRejection', (err) => {
-  // eslint-disable-next-line no-console
   console.log(err.name, err.message);
-  // eslint-disable-next-line no-console
   console.log('UNHANDLED REJECTION! 🎇 Shutting down');
   app.close(() => {
     process.exit(1);
