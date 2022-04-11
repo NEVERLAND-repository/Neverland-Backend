7c6cc5b5a5656525093188b26e77fc5e018a098b
Jim Ezesinachi <ezesinachijim@gmail.com>
2022-04-11 12:47:38 +0100

    bug fix

--- src/populate.js
+++ src/populate.js
@@ -6,16 +6,16 @@ const { Book } = require('./models');
 const jsonProducts = require('./books.json');
 
 const start = async () => {
-    try {
-        await connectDB(process.env.MONGO_URI);
-        console.log('Database Connection Is Successful!');
-        await Book.create(jsonProducts);
-        console.log('Database Population Is Successful!');
-        process.exit(0);
-    } catch (error) {
-        console.log(error);
-        process.exit(1);
-    }
-}
+  try {
+    await connectDB(process.env.MONGODB_URL);
+    console.log('Database Connection Is Successful!');
+    await Book.create(jsonProducts);
+    console.log('Database Population Is Successful!');
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
 
 start();
