29d6afa93f1a0d8d951d35f94e89ff653788d3a6
Jim Ezesinachi <ezesinachijim@gmail.com>
2022-04-18 13:21:57 +0100

    bug fix

--- src/routes/v1.js
+++ src/routes/v1.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const { homeControllers } = require('../controllers');
 const { baseRouter, authRouter, homeRouter } = require('./index');
 
 const app = express();
